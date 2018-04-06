83e8475dd371f537111038a57a8ca8fabc921dd5
joanaleao <joanacastroleao@gmail.com>
2018-04-06 17:16:22 +0100

    view va a descoberta

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,5 +1,16 @@
 class DishesController < ApplicationController
 
+  def index
+    @menus = Menu.all
+    @dishes_pet = Dish.where(meal_type:"Para Petiscar")
+    @dishes_ac = Dish.where(meal_type:"Para Acordar")
+    @dishes_aj = Dish.where(meal_type:"Para Almoçar | Jantar")
+    @dishes_acomp = Dish.where(meal_type:"Para Acompanhar")
+    @dishes_ter = Dish.where(meal_type:"Para Terminar")
+    @dishes_bbr = Dish.where(meal_type:"Para Beber")
+
+  end
+
   def show
   end
 
